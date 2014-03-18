7b8229e7db58dad605fc0cb2dc59762ac8379998
Jay Engineer <jayengineer6@gmail.com>
2014-03-18 17:26:32 -0400

    updated script

--- stats.py
+++ stats.py
@@ -12,6 +12,12 @@ import math
 
 def randomNumbers():
 
+    """
+
+    randomly generated data
+
+    """
+
     size = os.stat('numbers.txt').st_size #size of the file to make sure that it isn't filled already
 
     if size == 0:
@@ -26,17 +32,56 @@ def randomNumbers():
     return data
 
 def mean():
+
+    """
+
+    returns the mean/average of the sample data
+
+    """
+
+
     return (sum(randomNumbers())/len(randomNumbers()))
 
 def variance():
+    """
+
+    returns the variance of the sample data
+
+    """
+
     return sum(map(lambda x: x ** 2, [i-mean() for i in randomNumbers()]))/len(randomNumbers())
 
 def standardDeviation():
-    return math.sqrt(variance())
+    """
+
+    Returns standard deviation of the sample data
+
+    """
+
+    return round(math.sqrt(variance()))
+
+def withinSTD():
+    """
+
+    Returns the percent of numbers that are not part of the standard deviation
+
+    """
+
+    upperBound = mean() + standardDeviation()
+    lowerBound = mean() - standardDeviation()
+
+    numsNotInSTD = []
+
+    for i in randomNumbers():
+        if i > upperBound or i < lowerBound:
+            numsNotInSTD.append(i)
+
+    return float(len(numsNotInSTD))/float(len(randomNumbers()))*100
 
 def main():
     print mean() #57
     print variance() #725
     print standardDeviation() #26.9258240357
+    print withinSTD() # 40.0
 
 main()
