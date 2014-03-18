65473d9b4c216d6936c0112f3657026884a9b641
Jay Engineer <jayengineer6@gmail.com>
2014-03-18 02:10:30 -0400

    whitespace

--- stats.py
+++ stats.py
@@ -12,34 +12,31 @@ import math
 
 def randomNumbers():
 
-	size = os.stat('numbers.txt').st_size #size of the file to make sure that it isn't filled already
+    size = os.stat('numbers.txt').st_size #size of the file to make sure that it isn't filled already
 
-	if size == 0:
-		data = [randint(0,100) for i in range(50)]
-		filewriteOBJ = open('numbers.txt', 'w')
-		for i in data:
-			fileOBJ.write(str(i)+'\n')
-	else:
-		filereadOBJ = open('numbers.txt', 'r')
-		data = [int(i) for i in filereadOBJ]
+    if size == 0:
+        data = [randint(0,100) for i in range(50)]
+        filewriteOBJ = open('numbers.txt', 'w')
+        for i in data:
+            fileOBJ.write(str(i)+'\n')
+    else:
+        filereadOBJ = open('numbers.txt', 'r')
+        data = [int(i) for i in filereadOBJ]
 
-	return data
-
-# else:
-# 	print data
+    return data
 
 def mean():
-	return (sum(randomNumbers())/len(randomNumbers()))
+    return (sum(randomNumbers())/len(randomNumbers()))
 
 def variance():
-	return sum(map(lambda x: x ** 2, [i-mean() for i in randomNumbers()]))/len(randomNumbers())
+    return sum(map(lambda x: x ** 2, [i-mean() for i in randomNumbers()]))/len(randomNumbers())
 
 def standardDeviation():
-	return math.sqrt(variance())
+    return math.sqrt(variance())
 
 def main():
-	print mean() #57
-	print variance() #725
-	print standardDeviation() #26.9258240357
+    print mean() #57
+    print variance() #725
+    print standardDeviation() #26.9258240357
 
 main()
