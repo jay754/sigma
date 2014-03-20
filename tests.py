43fcede4eb120bce642a2939f7dbe5d3a9eed6a5
Jay Engineer <jayengineer6@gmail.com>
2014-03-20 03:03:23 -0400

    refactoring

--- tests.py
+++ tests.py
@@ -4,13 +4,13 @@ from stats import stdPy
 class stdTest(unittest.TestCase):
 
     def meanTest(self):
-        self.assertEqual(stdPy.mean(), 57)
+        self.assertEqual(stdPy().mean(),57)
 
     def varianceTest(self):
-        self.assertEqual(stdPy.variance(), 740)
+        self.assertEqual(stdPy().variance(),740)
 
     def stdTest(self):
-        self.assertEqual(stdPy.standardDeviation(), 27)
+        self.assertEqual(stdPy().standardDeviation(),27)
 
 if __name__ == '__main__':
     unittest.main()
